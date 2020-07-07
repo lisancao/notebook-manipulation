eab90f15b825c5a1a3975672b4bc0ab96759fa54
lisancao <lisanatashacao@gmail.com>
2020-07-07 14:12:05 -0600

    add converged files

---
+++ notebookmeta_generator/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  6 15:44:55 2020
+
+@author: lisacao
+"""
+
+from notebookdata import * 
+from notebookformatter import * 
+
---
+++ notebookmeta_generator/notebookformatter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  6 15:47:44 2020
+
+@author: lisacao
+"""
+
+
